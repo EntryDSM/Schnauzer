18564d5d4635270e86e855e07fc56200ce4f21ee
김대웅 <dkssud9556@dsm.hs.kr>
2020-06-12 15:16:46 +0900

    [FIX] error handler

--- schnauzer/src/app.ts
+++ schnauzer/src/app.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 import * as express from "express";
-import { Application } from "express";
+import { Application, NextFunction, Request, Response } from "express";
+import router from "./routes";
+import { HttpError } from "./error";
 
 class App {
   private readonly app: Application;
@@ -10,6 +12,20 @@ class App {
 
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: false }));
+    this.app.use("/", router);
+
+    this.app.use((req: Request, res: Response, next: NextFunction) => {
+      next(new HttpError("Not Found", 404));
+    });
+
+    this.app.use(
+      (err: HttpError, req: Request, res: Response, next: NextFunction) => {
+        res.status(err.status || 500).json({
+          message: err.message,
+          status: err.status,
+        });
+      }
+    );
   }
 
   public listen(port: number) {
