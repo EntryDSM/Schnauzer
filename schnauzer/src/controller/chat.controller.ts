0374a256dedaec716616f64a054b46956a7c8838
김대웅 <dkssud9556@dsm.hs.kr>
2020-06-14 01:55:57 +0900

    [FIX] email -> sub

--- schnauzer/src/controller/chat.controller.ts
+++ schnauzer/src/controller/chat.controller.ts
@@ -7,13 +7,13 @@ import { dbOptions } from "../config";
 
 export class ChatController {
   static getChats = async (req: Request, res: Response, next: NextFunction) => {
-    const { email }: { email: string } = res.locals.jwtPayload;
+    const { sub }: { sub: string } = res.locals.jwtPayload;
     const { page } = req.query;
     const connection = getConnection(dbOptions.CONNECTION_NAME);
     const userRepo = connection.getRepository(User);
     try {
-      if (await userRepo.findOne({ email })) {
-        const chats = await Qna.findByUserEmailWithPage(email, Number(page));
+      if (await userRepo.findOne({ email: sub })) {
+        const chats = await Qna.findByUserEmailWithPage(sub, Number(page));
         res.status(200).json(chats);
       } else {
         throw new HttpError("어드민 불허", 400);
