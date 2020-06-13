0a6ee3b272233f39ad39cc73037cfdaab29a33a5
김대웅 <dkssud9556@dsm.hs.kr>
2020-06-14 00:11:06 +0900

    [UPDATE] Paging in get chats controller

--- schnauzer/src/controller/chat.controller.ts
+++ schnauzer/src/controller/chat.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { createConnection, getConnection } from "typeorm";
+import { getConnection } from "typeorm";
 import { HttpError } from "../error";
 import { Qna } from "../entity/qna";
 import { User } from "../entity/user";
@@ -7,13 +7,13 @@ import { dbOptions } from "../config";
 
 export class ChatController {
   static getChats = async (req: Request, res: Response, next: NextFunction) => {
-    const connection = getConnection(dbOptions.CONNECTION_NAME);
     const { email }: { email: string } = res.locals.jwtPayload;
+    const { page } = req.query;
+    const connection = getConnection(dbOptions.CONNECTION_NAME);
     const userRepo = connection.getRepository(User);
-    const qnaRepo = connection.getRepository(Qna);
     try {
       if (await userRepo.findOne({ email })) {
-        const chats = await qnaRepo.find({ where: { user_email: email } });
+        const chats = await Qna.findByUserEmailWithPage(email, Number(page));
         res.status(200).json(chats);
       } else {
         throw new HttpError("어드민 불허", 400);
