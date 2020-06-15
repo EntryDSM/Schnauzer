6d1c35c52e75ba3951c9b9c137ba542a4531d1e9
김대웅 <dkssud9556@dsm.hs.kr>
2020-06-15 13:41:31 +0900

    [ADD] get chats with email controller

--- schnauzer/src/controller/chat.controller.ts
+++ schnauzer/src/controller/chat.controller.ts
@@ -4,21 +4,43 @@ import { HttpError } from "../error";
 import { Qna } from "../entity/qna";
 import { User } from "../entity/user";
 import { dbOptions } from "../config";
+import { Admin } from "../entity/admin";
 
 export class ChatController {
   static getChats = async (req: Request, res: Response, next: NextFunction) => {
-    const { sub }: { sub: string } = res.locals.jwtPayload;
+    const userEmail = res.locals.jwtPayload.sub;
     const { page } = req.query;
-    const connection = getConnection(dbOptions.CONNECTION_NAME);
-    const userRepo = connection.getRepository(User);
     try {
-      if (await userRepo.findOne({ email: sub })) {
-        const chats = await Qna.findByUserEmailWithPage(sub, Number(page));
-        res.status(200).json(chats);
-      } else {
+      const connection = getConnection(dbOptions.CONNECTION_NAME);
+      const userRepo = connection.getRepository(User);
+      if (!(await userRepo.findOne({ email: userEmail }))) {
         throw new HttpError("어드민 불허", 400);
       }
+      const chats = await Qna.findByUserEmailWithPage(userEmail, Number(page));
+      res.status(200).json(chats);
     } catch (e) {
+      next(e);
+    }
+  };
+
+  static getChatsWithEmail = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const adminEmail = res.locals.jwtPayload.sub;
+    const userEmail = req.params.email;
+    const { page } = req.query;
+    try {
+      const connection = getConnection(dbOptions.CONNECTION_NAME);
+      const adminRepo = connection.getRepository(Admin);
+      if (!(await adminRepo.findOne({ email: adminEmail }))) {
+        throw new HttpError("일반 유저 불허", 400);
+      }
+      const chats = await Qna.findByUserEmailWithPage(userEmail, Number(page));
+      res.status(200).json(chats);
+    } catch (e) {
+      console.log(e);
       next(e);
     }
   };
