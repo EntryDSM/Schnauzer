388bd44cfcc5c87634474d9cbfc66f3e29d79e9e
김대웅 <dkssud9556@dsm.hs.kr>
2020-06-18 14:37:26 +0900

    [UPDATE] search router

--- schnauzer/src/routes/index.ts
+++ schnauzer/src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { ChatController } from "../controller/chat.controller";
 import jwtCheck from "../middleware/jwtCheck";
+import { SearchController } from "../controller/search.controller";
 
 const router = Router();
 
 router.get("/chats", jwtCheck, ChatController.getChats);
 router.get("/chats/:email", jwtCheck, ChatController.getChatsWithEmail);
 router.get("/last-chats", jwtCheck, ChatController.getLastChats);
+router.get("/search/:name", jwtCheck, SearchController.searchByName);
 
 export default router;
