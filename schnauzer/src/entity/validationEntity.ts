255ada0df2088ecaf84605870877775c48fa4af7
김대웅 <dkssud9556@dsm.hs.kr>
2020-06-25 14:17:11 +0900

    [ADD] throw http error

--- schnauzer/src/entity/validationEntity.ts
+++ schnauzer/src/entity/validationEntity.ts
@@ -1,10 +1,15 @@
 import { BaseEntity, BeforeInsert, BeforeUpdate } from "typeorm";
 import { validateOrReject } from "class-validator";
+import { HttpError } from "../error";
 
 export abstract class ValidationEntity extends BaseEntity {
   @BeforeInsert()
   @BeforeUpdate()
   async validate(): Promise<void> {
-    await validateOrReject(this);
+    try {
+      await validateOrReject(this);
+    } catch (e) {
+      throw new HttpError("파라미터 부정확", 400);
+    }
   }
 }
