555913069bc5b45b0bedfb837701312e0e3a3dea
김대웅 <dkssud9556@dsm.hs.kr>
2020-07-06 20:52:08 +0900

    [ADD] cors

--- schnauzer/src/app.ts
+++ schnauzer/src/app.ts
@@ -1,13 +1,14 @@
 import "reflect-metadata";
 import * as express from "express";
 import { Application, NextFunction, Request, Response } from "express";
+import * as SocketIO from "socket.io";
+import { Server as IO } from "socket.io";
+import { Server, createServer } from "http";
+import * as cors from "cors";
+import socketInit from "./socket/index";
 import router from "./routes";
 import { HttpError } from "./error";
 import { errorHandler } from "./middleware/errorHandler";
-import * as SocketIO from "socket.io";
-import { Server as IO } from "socket.io";
-import socketInit from "./socket/index";
-import { Server, createServer } from "http";
 
 class App {
   private app: Application;
@@ -31,6 +32,7 @@ class App {
     this.app = express();
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(cors());
   }
 
   private createServer(): void {
